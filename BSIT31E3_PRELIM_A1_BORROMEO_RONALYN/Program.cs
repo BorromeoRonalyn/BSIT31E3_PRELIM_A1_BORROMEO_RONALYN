b68c81d34e32b54ccae0840a2dac369cf283b6ae
BORROMEO, RONALYN, MIRANDA <1518-22@itmlyceumalabang.onmicrosoft.com>
2026-06-20 08:35:19 +0800

    Final testing and cleanup

--- BSIT31E3_PRELIM_A1_BORROMEO_RONALYN/Program.cs
+++ BSIT31E3_PRELIM_A1_BORROMEO_RONALYN/Program.cs
@@ -1,7 +1,9 @@
 ﻿// Commit 2: Added loop structure
 // Commit 3: Implemented FizzBuzz logic.
+// Commit 4: Final testing and cleanup
 
 for (int i = 1; i <= 100; i++)
+    //LOGIC BELOW
 {
     if (i % 3 == 0 && i % 5 == 0)
     {
